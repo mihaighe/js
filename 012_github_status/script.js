983dd148daa680337f0e91b38b911262d8a0734a
Mihai Ghe <mihai.md16@gmail.com>
2022-03-25 20:48:11 +0200

    Add github status project

---
+++ 012_github_status/script.js
@@ -0,0 +1,12 @@
+// const requestButton = document.querySelector('.request-button');
+
+// requestButton.addEventListener('click', () => {
+//   console.log('clicked');
+
+//   fetch('https://www.githubstatus.com/api/v2/summary.json')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// });
+
+// // https://www.githubstatus.com/api/v2/summary.json
+// // https://www.githubstatus.com/api/v2/status.json
